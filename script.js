548e8bf8c534b7cc5e18e92d9ca3cc23dfe366e2
abdoessordo <62575309+abdoessordo@users.noreply.github.com>
2020-05-16 16:41:44 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const color2 = document.getElementById('color2')
 const body = document.getElementById('body')
 
 function setGradiant(){
+    var text = "linear-gradient(to right, "+ color1.value + ", " + color2.value + ")"
     body.style.background = 
     "linear-gradient(to right,"
      + color1.value + "," + color2.value + ")"
+
+    h3.innerHTML = text
 }
 
 color1.addEventListener("input", setGradiant);
 
 color2.addEventListener("input", setGradiant);
 
-var text = "linear-gradient(to right, "+ color1.value + ", " + color2.value + ")"
-
-h3.innerHTML = text
